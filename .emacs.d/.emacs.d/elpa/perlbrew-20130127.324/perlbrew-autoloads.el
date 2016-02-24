9bb06d8847af0ee44024f41d6aa97d7761e457a9
Logan Bell <loganbell@gmail.com>
2016-02-23 23:34:03 -0800

    Removing a spurious folder

--- .emacs.d/.emacs.d/elpa/perlbrew-20130127.324/perlbrew-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; perlbrew-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("perlbrew.el") (21653 46317 483717 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; perlbrew-autoloads.el ends here
